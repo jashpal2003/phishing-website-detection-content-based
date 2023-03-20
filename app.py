6efa92aed5b2ebf61f15b2b0f8df7be07d2fd2ff
jashpal2003 <60477148+jashpal2003@users.noreply.github.com>
2023-03-20 22:43:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,77 +6,12 @@ import requests as re
 import matplotlib.pyplot as plt
 
 # col1, col2 = st.columns([1, 3])
+st.title('JT Hunters')
+st.header('Phishing Website Detection using Machine Learning')
+st.write(' hamare user bahut samajdar hai inshort usko ML algorithm pata hai. aap url enter karke check kar sakte hai ki website legitimate ya phishing website hai  "_. ')
 
-st.title('Phishing Website Detection using Machine Learning')
-st.write('This ML-based app is developed for educational purposes. Objective of the app is detecting phishing websites only using content data. Not URL!'
-         ' You can see the details of approach, data set, and feature set if you click on _"See The Details"_. ')
-
-
-with st.expander("PROJECT DETAILS"):
-    st.subheader('Approach')
-    st.write('I used _supervised learning_ to classify phishing and legitimate websites. '
-             'I benefit from content-based approach and focus on html of the websites. '
-             'Also, I used scikit-learn for the ML models.'
-             )
-    st.write('For this educational project, '
-             'I created my own data set and defined features, some from the literature and some based on manual analysis. '
-             'I used requests library to collect data, BeautifulSoup module to parse and extract features. ')
-    st.write('The source code and data sets are available in the below Github link:')
-    st.write('_https://github.com/emre-kocyigit/phishing-website-detection-content-based_')
-
-    st.subheader('Data set')
-    st.write('I used _"phishtank.org"_ & _"tranco-list.eu"_ as data sources.')
-    st.write('Totally 26584 websites ==> **_16060_ legitimate** websites | **_10524_ phishing** websites')
-    st.write('Data set was created in October 2022.')
-
-    # ----- FOR THE PIE CHART ----- #
-    labels = 'phishing', 'legitimate'
-    phishing_rate = int(ml.phishing_df.shape[0] / (ml.phishing_df.shape[0] + ml.legitimate_df.shape[0]) * 100)
-    legitimate_rate = 100 - phishing_rate
-    sizes = [phishing_rate, legitimate_rate]
-    explode = (0.1, 0)
-    fig, ax = plt.subplots()
-    ax.pie(sizes, explode=explode, labels=labels, shadow=True, startangle=90, autopct='%1.1f%%')
-    ax.axis('equal')
-    st.pyplot(fig)
-    # ----- !!!!! ----- #
-
-    st.write('Features + URL + Label ==> Dataframe')
-    st.markdown('label is 1 for phishing, 0 for legitimate')
-    number = st.slider("Select row number to display", 0, 100)
-    st.dataframe(ml.legitimate_df.head(number))
-
-
-    @st.cache
-    def convert_df(df):
-        # IMPORTANT: Cache the conversion to prevent computation on every rerun
-        return df.to_csv().encode('utf-8')
-
-    csv = convert_df(ml.df)
-
-    st.download_button(
-        label="Download data as CSV",
-        data=csv,
-        file_name='phishing_legitimate_structured_data.csv',
-        mime='text/csv',
-    )
-
-    st.subheader('Features')
-    st.write('I used only content-based features. I didn\'t use url-based faetures like length of url etc.'
-             'Most of the features extracted using find_all() method of BeautifulSoup module after parsing html.')
-
-    st.subheader('Results')
-    st.write('I used 7 different ML classifiers of scikit-learn and tested them implementing k-fold cross validation.'
-             'Firstly obtained their confusion matrices, then calculated their accuracy, precision and recall scores.'
-             'Comparison table is below:')
-    st.table(ml.df_results)
-    st.write('NB --> Gaussian Naive Bayes')
-    st.write('SVM --> Support Vector Machine')
-    st.write('DT --> Decision Tree')
-    st.write('RF --> Random Forest')
-    st.write('AB --> AdaBoost')
-    st.write('NN --> Neural Network')
-    st.write('KN --> K-Neighbours')
+st.write("WARNING : agar satisfy result na mile to samaj lo ki aap itne samajdar user nahi ho jo best algorithm select na kar sake ")
+st.write("SIDHI BAAT :- tu ye site use karne ke layak nahi hai ")
 
 with st.expander('EXAMPLE PHISHING URLs:'):
     st.write('_https://rtyu38.godaddysites.com/_')
