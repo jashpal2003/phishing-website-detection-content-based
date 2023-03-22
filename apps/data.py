08bb6f3e8d98fe38378f43778d9aab174e1cb5d0
jashpal2003 <60477148+jashpal2003@users.noreply.github.com>
2023-03-22 19:25:01 +0530

    Create data.py

---
+++ apps/data.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def app():
+     st.title('STORIES & EXAMPLES')
+
+     st.header("Gilly")
+
+     st.write("Gilly is an IU employee. Today she has back-to-back meetings. While checking emails on her phone, she sees an email from the “Employee Portal” prompting her to confirm payment for a medical procedure immediately. She clicks on the link and is taken to a site that looks much like IU’s Central Authentication Service (CAS) login page—except that the form didn't seem to behave the same as usual when entering her username and passphrase.Gilly submits her IU login details and is then redirected to a strange page. She is suddenly distracted when the organizer of her next meeting walks by, and she forgets about the message.")
+     st.header("Gordon")
+     st.write("Gordon is a new manager-level employee at IU. One week after starting his position, he receives an email that appears to be from “The IT Support Center” which requires that he verify his “company password” is strong enough as a new employee.The email has a link that takes Gordon to an IU-branded page with a form to enter his email address and password so it can be verified. Gordon wants to make sure his passphrase is strong, so he fills out the form.Connected to his position, Gordon has already been added to a Box folder that contains restricted institutional data.")
+     st.header("Garmin")
+     st.write("Garmin is an IU graduate student in his final year. He receives a message that seems to be from the head of his department, recommending he attend an upcoming event.He clicks the “Register” link, which results in a “page not found” notice on an unknown website.Garmin doesn’t realize that by clicking the link, a keylogging malware application was installed on his computer. Later, from that same computer, he makes some online purchases.")
