a2ec2d74f386719f17b51914472fcc360676692b
jashpal2003 <60477148+jashpal2003@users.noreply.github.com>
2023-03-22 19:25:44 +0530

    Create prevent.py

---
+++ apps/prevent.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def app():
+    st.title('Prevent Phishing Attacks:')
+
+    st.write('To protect against spam mails, spam filters can be used. Generally, the filters assess the origin of the message, the software used to send the message, and the appearance of the message to determine if its spam. Occasionally, spam filters may even block emails from legitimate sources, so it isnt always 100% accurate.')
+
+    st.write('The browser settings should be changed to prevent fraudulent websites from opening. Browsers keep a list of fake websites and when you try to access the website, the address is blocked or an alert message is shown. The settings of the browser should only allow reliable websites to open up.')
+    st.write('Many websites require users to enter login information while the user image is displayed. This type of system may be open to security attacks. One way to ensure security is to change passwords on a regular basis, and never use the same password for multiple accounts. Its also a good idea for websites to use a CAPTCHA system for added security.')
+    st.write('Banks and financial organizations use monitoring systems to prevent phishing. Individuals can report phishing to industry groups where legal actions can be taken against these fraudulent websites. Organizations should provide security awareness training to employees to recognize the risks.')
+    st.write('Changes in browsing habits are required to prevent phishing. If verification is required, always contact the company personally before entering any details online.')
