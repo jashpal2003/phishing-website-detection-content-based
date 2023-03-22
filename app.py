1dc68eabf989f01acb624c9a024f2f13093cd624
jashpal2003 <60477148+jashpal2003@users.noreply.github.com>
2023-03-22 19:23:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,60 +2,35 @@ import streamlit as st
 import machine_learning as ml
 import feature_extraction as fe
 from bs4 import BeautifulSoup
-import requests as re
+import requests as me
 import matplotlib.pyplot as plt
+import re
+from multiapp import MultiApp
+from apps import data,prevent # import your app modules here
+
 
 # col1, col2 = st.columns([1, 3])
 st.title('JT Hunters')
 st.header('Phishing Website Detection using Machine Learning')
-st.write(' hamare user bahut samajdar hai inshort usko ML algorithm pata hai. aap url enter karke check kar sakte hai ki website legitimate ya phishing website hai  "_. ')
 
-st.write("WARNING : agar satisfy result na mile to samaj lo ki aap itne samajdar user nahi ho jo best algorithm select na kar sake ")
-st.write("SIDHI BAAT :- tu ye site use karne ke layak nahi hai ")
 
 with st.expander('EXAMPLE PHISHING URLs:'):
     st.write('_https://rtyu38.godaddysites.com/_')
     st.write('_https://karafuru.invite-mint.com/_')
     st.write('_https://defi-ned.top/h5/#/_')
     st.caption('REMEMBER, PHISHING WEB PAGES HAVE SHORT LIFECYCLE! SO, THE EXAMPLES SHOULD BE UPDATED!')
+st.subheader('here we use "Random forest" ML model ')
 
-choice = st.selectbox("Please select your machine learning model",
-                 [
-                     'Gaussian Naive Bayes', 'Support Vector Machine', 'Decision Tree', 'Random Forest',
-                     'AdaBoost', 'Neural Network', 'K-Neighbours'
-                 ]
-                )
 
-model = ml.nb_model
-
-if choice == 'Gaussian Naive Bayes':
-    model = ml.nb_model
-    st.write('GNB model is selected!')
-elif choice == 'Support Vector Machine':
-    model = ml.svm_model
-    st.write('SVM model is selected!')
-elif choice == 'Decision Tree':
-    model = ml.dt_model
-    st.write('DT model is selected!')
-elif choice == 'Random Forest':
-    model = ml.rf_model
-    st.write('RF model is selected!')
-elif choice == 'AdaBoost':
-    model = ml.ab_model
-    st.write('AB model is selected!')
-elif choice == 'Neural Network':
-    model = ml.nn_model
-    st.write('NN model is selected!')
-else:
-    model = ml.kn_model
-    st.write('KN model is selected!')
+model = ml.rf_model
 
 
 url = st.text_input('Enter the URL')
+
 # check the url is valid or not
 if st.button('Check!'):
     try:
-        response = re.get(url, verify=False, timeout=4)
+        response = me.get(url, verify=False, timeout=4)
         if response.status_code != 200:
             print(". HTTP connection was not successful for the URL: ", url)
         else:
@@ -69,9 +44,20 @@ if st.button('Check!'):
                 st.warning("Attention! This web page is a potential PHISHING!")
                 st.snow()
 
-    except re.exceptions.RequestException as e:
+    except me.exceptions.RequestException as e:
         print("--> ", e)
 
+
+
+app = MultiApp()
+
+
+# Add all your application here
+
+app.add_app("STORIES & EXAMPLES", data.app)
+app.add_app("how to prevent phising", prevent.app)
+# The main app
+app.run()
 
 
 
